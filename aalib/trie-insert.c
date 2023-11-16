114dc54347920ee507a395d555604dcf929f9a1b
Lukas K <lkrampit@uoguelph.ca>
2023-11-15 23:04:20 -0500

    add the ability to insert full seperate chains

--- aalib/trie-insert.c
+++ aalib/trie-insert.c
@@ -6,7 +6,7 @@
 
 #include "trie_defs.h"
 
-
+static int trie_add_chain(TrieNode ***, int, AAKeyType, TrieNode *);
 
 /** compare a key with the letter in a node */
 int
@@ -24,11 +24,29 @@ trie_subtreeSearchComparator(const void *keyValue, const void *nodePtr)
  * NOTE: This functions IS explained in the lecture slides
  */
 static TrieNode *
-trie_create_chain(AAKeyType key, size_t keylength, void *value, int *cost)
+trie_create_chain(AAKeyType key, int keyStartPos , size_t keylength, void *value, int *cost)
 {
 	TrieNode *current = NULL;
 
-	// TO DO: create a new chain the the required key letters
+	// DONE: create a new chain the the required key letters
+
+	// TO DO: do something with this cost variable
+
+	current = trieCreateNode();
+
+	current->letter = (TrieLetter)key[keyStartPos];
+
+	if (keylength - keyStartPos == 1)
+	{
+		current->value = value;
+		current->isKeySoHasValue = 1; //set this to being a valid key
+	}
+	else
+	{
+		keyStartPos++;
+		// recurse
+		current->nSubtries = trie_add_chain(&(current->subtries), current->nSubtries, &(key[keyStartPos]), trie_create_chain(key, keyStartPos, keylength, value, cost));
+	}
 
 	return current;
 }
@@ -39,18 +57,42 @@ trie_create_chain(AAKeyType key, size_t keylength, void *value, int *cost)
  */
 static int
 trie_add_chain(
-		TrieNode **subtreeList, int nSubtries,
+		TrieNode ***subtreeListPTR, int nSubtries,
 		AAKeyType key,
 		TrieNode *newChain
 	)
 {
-	// TO DO:  add the provided chain to the node; the first letter
+	// DONE: add the provided chain to the node; the first letter
 	// of the key may be used to organize where within the set of
 	// subtries you add this
 
-	// TO DO: you probably want to replace this return statement
+	// first check if this is a new subtrie
+	if ((*subtreeListPTR) == NULL)
+	{
+		(*subtreeListPTR) = malloc(sizeof(TrieNode*));
+		//count this as the first subtrie
+		nSubtries = 1;
+
+	}
+	else
+	{
+		// first increase the size of the subtree list
+		// also increment nSubtries by 1 to account for the larger size now
+		(*subtreeListPTR) = realloc((*subtreeListPTR), (sizeof(TrieNode *) * (++nSubtries)));
+	}
+
+	/**
+	 * TO DO: Use a Binary Search to add this in
+	 * Assume the subtreeList is already sorted because all the others chains will have been added this way
+	 */
+	// just append it for now cause that's easier
+	(*subtreeListPTR)[nSubtries - 1] = newChain;
+
+	//printf("\nChecking added chain of key: %c\n", (*subtreeListPTR)[0]->letter);
+
+	// DONE: you probably want to replace this return statement
 	// with your own code
-	return -1;
+	return nSubtries;
 }
 
 
@@ -86,15 +128,40 @@ trieInsertKey(KeyValueTrie *root,
 		root->maxKeyLength = keylength;
 
 	if (root->nSubtries == 0) {
+		// if this is 0 then there is no table yet
+		// first allocate the table
+		// start it at size 1 becasue that way the nSubtries variable will always be able to tell us how many nodes are direct children
+		// this is decided to be the best option because I believe we were told not to modify the API so I cannot add a new varible that
+		// keeps track of the size of the array seperate from nSubtries.
+		root->subtries = (TrieNode **)malloc(sizeof(TrieNode *)); // give it a size of 1 to start
+
 		root->subtries[root->nSubtries++] = trie_create_chain(
-				key, keylength, value, cost);
+				key, 0, keylength, value, cost);
 		return 0;
 	}
+	else // If the trie is not empty and there are already existing chains
+	{
+		// TO DO: find the subtrie with the leading letter of the
+		// key, and insert the new key into the correct subtrie
+		// chain based on that letter
 
-	
-	// TO DO: find the subtrie with the leading letter of the
-	// key, and insert the new key into the correct subtrie
-	// chain based on that letter
+		TrieNode *chain = trie_search_for_matching_chain(root->subtries, root->nSubtries, key[0]);
+
+		// check to see if we found a matching chain that already exists
+		if (chain == NULL)
+		{
+			// add the new chain to the table of subtries
+			root->nSubtries = trie_add_chain(&(root->subtries), root->nSubtries, key, trie_create_chain(key, 0, keylength, value, cost));
+
+			return root->nSubtries;
+		}
+		else
+		{
+			// link-to-chain(c, κ, l, v)
+			//a temp call to trie_link_to_chain()
+			trie_link_to_chain(NULL, NULL, 0, NULL, NULL);
+		}
+	}
 
 	// TO DO: you probably want to replace this return statement
 	// with your own code
